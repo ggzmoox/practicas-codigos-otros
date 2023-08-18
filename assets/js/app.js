798b88393fe15a3a9d3b54eabf0f6bbf4142153a
davidOlea <luis.david.olea.mtz@gmail.com>
2023-08-18 10:34:17 -0600

    se corrigen algunos errores

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,18 @@
+// se quitaron los signos de pesos de las variables
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+const n = document.querySelector('name');
+const b = document.querySelector('#blog');
+const l = document.querySelector('.location');
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
+async function displayUser (username) {
+  n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  // se cambaron las comillas simples por comilla inversa
+  n.textContent = `${data.name}`;
+  b.textContent = `${data.blog}`;
+  l.textContent = `${data.location}`;
 }
 
 function handleError(err) {
